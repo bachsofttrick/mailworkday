c37749d0cf40dbf14524465b046a08d2c4af3a56
bachsofttrick <xuanbach1307@gmail.com>
2021-10-31 00:07:51 +0700

    unlock dayJobRun

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const config = require('./appConfig.json');
 const { weekdays, months } = require('./translate');
 
 function getOccuringTimesOfDayOfWeek(dayOfWeek, date = null) {
-    // dayOfWeek: Sunday is 0, Monday is 1, and so on.
-    date = date || new Date();
-    // reset to 0h
-    date = new Date(date.toDateString());
-    const firstDateOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
-    const nextWeekDayOfFirstDate = getNextDayOfWeek(dayOfWeek, firstDateOfMonth);
-    const goToWorkWeekendDay = getNextDayOfWeek(dayOfWeek, date);
-    let weeks = 1;
-    if (date.getMonth() === goToWorkWeekendDay.getMonth() && date >= nextWeekDayOfFirstDate) {
-        weeks += parseInt(1 + ((date - nextWeekDayOfFirstDate) / (86400000*7)));
-    }
+  // dayOfWeek: Sunday is 0, Monday is 1, and so on.
+  date = date || new Date();
+  // reset to 0h
+  date = new Date(date.toDateString());
+  const firstDateOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+  const nextWeekDayOfFirstDate = getNextDayOfWeek(dayOfWeek, firstDateOfMonth);
+  const goToWorkWeekendDay = getNextDayOfWeek(dayOfWeek, date);
+  let weeks = 1;
+  if (date.getMonth() === goToWorkWeekendDay.getMonth() && date >= nextWeekDayOfFirstDate) {
+      weeks += parseInt(1 + ((date - nextWeekDayOfFirstDate) / (86400000*7)));
+  }
 
   return [weeks, goToWorkWeekendDay];
 }
@@ -29,15 +29,15 @@ function getNextDayOfWeek(dayOfWeek, date = null) {
 }
 
 function translateDate(date = null) {
-    date = date || new Date();
-    const dateArray = date.toDateString().split(' ');
-    return `${weekdays.find((i) => i.name === dateArray[0]).translate} ngày ${dateArray[2]} tháng ${months.indexOf(dateArray[1]) + 1} năm ${dateArray[3]}`;
+  date = date || new Date();
+  const dateArray = date.toDateString().split(' ');
+  return `${weekdays.find((i) => i.name === dateArray[0]).translate} ngày ${dateArray[2]} tháng ${months.indexOf(dateArray[1]) + 1} năm ${dateArray[3]}`;
 }
 
 // only 1 day before get send email
 const now = new Date();
 if (now.getDay() !== config.dayJobRun) {
-  // return;
+  return;
 }
 
 const workWeekend = getOccuringTimesOfDayOfWeek(config.workWeekendDay, now);
