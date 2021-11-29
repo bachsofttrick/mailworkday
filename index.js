96a751d0422d0062a2566d01c86bc581ec48d4f1
bachsofttrick <xuanbach1307@gmail.com>
2021-11-29 23:25:28 +0700

    split dayRunJob to 2 separate

--- index.js
+++ index.js
@@ -2,18 +2,14 @@ const nodemailer = require('nodemailer');
 const config = require('./appConfig.json');
 const { getOccuringTimesOfDayOfWeek, translateDate } = require('./lib');
 const writeWorkEvent = require('./g-calendar');
-
-// only 1 day before get send email
 const now = new Date();
-if (now.getDay() !== config.dayJobRun) {
-  return;
-}
 
 const workWeekend = getOccuringTimesOfDayOfWeek(config.workWeekendDay, now);
 const goToWorkSat = config.weekGoToWork.includes(workWeekend[0]);
 const notiBody = `Tuần này ${translateDate(workWeekend[1])} là tuần thứ ${workWeekend[0]}${goToWorkSat ? '' : ' không'} phải đi làm nha!`;
 // send email
-if (config.email.enable) {
+// only 1 day before get send email
+if (config.email.enable && now.getDay() === config.email.dayJobRun) {
   const transporter = nodemailer.createTransport(config.email.nodemailer);
   transporter.sendMail({
       from: config.email.from,
@@ -29,6 +25,6 @@ if (config.email.enable) {
 }
 
 // set event in Google calendar
-if (config.g_calendar.enable && goToWorkSat) {
+if (config.g_calendar.enable && now.getDay() === config.g_calendar.dayJobRun && goToWorkSat) {
   writeWorkEvent(workWeekend[1], notiBody);
 }
